dbbae5ff1f3609a12375630d8687311ab5a95250
Romain Maneschi <romain@maneschi.fr>
2016-05-09 21:50:46 +0200

    general display is ok

--- app/client/theory/app.ts
+++ app/client/theory/app.ts
@@ -1,16 +1,67 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import {Component} from "@angular/core";
+import {Component, DynamicComponentLoader, ElementRef, Injector, Type, ViewChild, HostListener} from "@angular/core";
+import {Http, HTTP_PROVIDERS, Response} from '@angular/http';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.html'//relative to app/client
+    template: `
+        <div class="slideContainer" #slide (keypress)="eventHandler($event)"></div>
+        <footer>
+            <span class="github"><i class="fa fa-github-alt" aria-hidden="true"></i><a href="https://github.com/manland/angular2-bien-ou-pas-bien">manland/angular2-bien-ou-pas-bien</a></span>
+            <span class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i><a href="https://twitter.com/RmManeschi">@RmManeschi</a></span>
+            <span class="slideIndex">{{currentSlideIndex}}/{{ID_SLIDE_MAX}}</span>
+        </footer>
+    `
 })
 export class App {
 
-    constructor() {
+    private ID_SLIDE_MIN: number = 0;
+    private ID_SLIDE_MAX: number = 2;
 
+    @ViewChild('slide', true)
+    slide: ElementRef;
+
+    private currentSlideIndex: number;
+
+    constructor(private http: Http) {
+        const slideNb = window.location.hash.substr(1);//TODO replace this hack by adding router
+        this.currentSlideIndex = this.ID_SLIDE_MIN;
+        if(slideNb) {
+            try {
+                this.currentSlideIndex = parseInt(slideNb, 10);
+            } catch(e) {}
+        }
+        this.display();
     }
+
+    display() {
+        this.http.get(`slides/slide${this.currentSlideIndex}.html`).subscribe((response: Response) => this.slide.nativeElement.innerHTML = response.text());
+    }
+
+    @HostListener('window:keydown', ['$event'])
+    keyboardInput(event: any) {
+        let currentSlideIndex = this.currentSlideIndex;
+
+        if(event.keyCode === 39) {
+            currentSlideIndex++;
+        } else if(event.keyCode === 37) {
+            currentSlideIndex--;
+        }
+
+        if(currentSlideIndex !== this.currentSlideIndex) {
+            if (currentSlideIndex < this.ID_SLIDE_MIN) {
+                currentSlideIndex = this.ID_SLIDE_MIN;
+            } else if (currentSlideIndex > this.ID_SLIDE_MAX) {
+                currentSlideIndex = this.ID_SLIDE_MAX;
+            }
+            this.currentSlideIndex = currentSlideIndex;
+            this.display();
+            window.location.hash = `${this.currentSlideIndex}`;//TODO replace this hack by adding router
+        }
+    }
+
 
 }
 
-bootstrap(App, []);
+
+bootstrap(App, [HTTP_PROVIDERS]);
